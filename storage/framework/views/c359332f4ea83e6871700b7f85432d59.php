e55d0030b0f2e72a0265788c4153978c663b1a23
culniga <culniga@gmail.com>
2024-07-27 14:07:15 +0300

    Gitignore update

--- storage/framework/views/c359332f4ea83e6871700b7f85432d59.php
+++
@@ -1,3 +0,0 @@
-<?php echo e(htmlentities($value)); ?>
-
-<input class="inp" id="<?php echo e($model->id); ?>" style="width:40px; display:none; font-size:11px; text-align:center;" type="hidden" value="<?php echo e($model->pos); ?>" rel="<?php echo e($table); ?>" cat="" cat_val="" /><?php /**PATH /home/ubuntu/theshit_php/Modules/Core/Providers/../Resources/views/list_fields/pos.blade.php ENDPATH**/ ?>
