ce83141d1e157d7e05c7822e3cc51e095e4a3ae5
Алексей Апостолов <alex@c-tm.ru>
2024-08-14 21:21:56 +0300

    pass projects tasks

--- app/Api/ProjectsApiController.php
+++ app/Api/ProjectsApiController.php
@@ -4,7 +4,14 @@ namespace App\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\RedisService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Modules\Accounts\Entities\Account;
+use Modules\PartnersQuests\Entities\AccountPartnersQuest;
+use Modules\Projects\Entities\AccountProjectTask;
 use Modules\Projects\Entities\Project;
+use Modules\Projects\Entities\ProjectTask;
 
 class ProjectsApiController extends Controller
 {
@@ -140,5 +147,64 @@ class ProjectsApiController extends Controller
         });
 
         return response()->json($data, 201);
+    }
+
+    public function pass_projects_quest(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'token' => 'required',
+            'user_id' => 'required|regex:/^[0-9]+$/u',
+            'projects_task_id' => 'required|regex:/^[0-9]+$/u'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 404);
+        }
+
+        if (!$this->isTokenValid($request->post('token'))) {
+            return response()->json(['message' => 'token invalid'], 404);
+        }
+
+        $projectsTaskId = $request->post('projects_task_id');
+        $userId = $request->post('user_id');
+
+        $projectTask = ProjectTask::find($projectsTaskId);
+        $account = Account::find($userId, ['id', 'wallet_balance']);
+
+        if (!$projectTask) {
+            return response()->json(['message' => 'Project task not found'], 404);
+        }
+
+        if ($account === null) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        if (AccountProjectTask::where('projects_task_id', $projectsTaskId)->exists()) {
+            return response()->json(['message' => 'Project task already passed'], 404);
+        }
+
+        DB::transaction(function () use ($projectTask, $account) {
+            $accountProjectTask = new AccountProjectTask();
+            $accountProjectTask->account_id = $account->id;
+            $accountProjectTask->projects_task_id = $projectTask->id;
+            $accountProjectTask->save();
+
+            $account->wallet_balance += $projectTask->reward;
+            $account->save();
+        });
+
+        $updatedAccount = Account::with(['daily_quests', 'partners_quests', 'projects_tasks:account_id,projects_task_id'])->find($userId);
+
+        if ($updatedAccount->id_telegram) {
+            $redis = new RedisService();
+            $redis->updateIfNotSet($updatedAccount->id_telegram, $updatedAccount->toJson(), $updatedAccount->timezone);
+        }
+
+        return response()->json($updatedAccount, 201);
+    }
+
+    private function isTokenValid($token)
+    {
+        return checkToken($token);
     }
 }
